23246c794bd667e258574672a148bff740a11fb1
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-06-13 19:23:20 -0500

    firsste

---
+++ android/app/src/main/kotlin/com/example/whatsappg12/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.whatsappg12
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:whatsappg12/pages/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomePage(), debugShowCheckedModeBanner: false));
+}
